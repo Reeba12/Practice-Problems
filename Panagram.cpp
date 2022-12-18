a15d2508af98b578d22140095fbd78b44efa765e
Reeba12 <reebasiddiqui456@gmail.com>
2022-12-19 02:19:07 +0500

    feat: Pangram for all testcases

--- Panagram.cpp
+++ Panagram.cpp
@@ -11,36 +11,73 @@ using namespace std;
 
 string pangrams(string s)
 {
-    int check = 0;
-    sort(s.begin(), s.end());
-    for (int i = 0; i < s.size(); i++)
-    {
-        putchar(tolower(s[i]));
-        if (s[i] != s[i + 1])
-        {
-            check++;
-        }
-    }
+    // int check = 0;
+    // sort(s.begin(), s.end());
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     putchar(tolower(s[i]));
+    //     if (s[i] != s[i + 1])
+    //     {
+    //         check++;
+    //     }
+    // }
 
-    if (check >= 27)
+    // if (check >= 27)
+    // {
+    //     return "pangram";
+    // }
+    // else
+    //     return "not pangram";
+    
+    // METHOD # 2 FOR ALL TEST CASES
+    int count = 0;
+    vector<bool> mark(26, false);
+    int index;
+    // Traverse all characters
+    for (int i = 0; i < s.length(); i++)
     {
-        return "pangram";
+        // If uppercase character, subtract 'A'
+        // to find index.
+        if ('A' <= s[i] && s[i] <= 'Z')
+        {
+            index = s[i] - 'A';
+            cout << index << endl;
+        }
+        // If lowercase character, subtract 'a'
+        // to find index.
+        else if ('a' <= s[i] && s[i] <= 'z')
+        {
+            index = s[i] - 'a';
+            cout << index << endl;
+        }
+        mark[index] = true;
+    }
+    for (int i = 0; i <= 25; i++)
+    {
+        if (mark[i] == false)
+        {
+            count++;
+        };
+    }
+    if (count)
+    {
+        return "not pangram";
     }
     else
-        return "not pangram";
+        return "pangram";
 }
 int main()
 {
-    ofstream fout(getenv("OUTPUT_PATH"));
+    // ofstream fout(getenv("OUTPUT_PATH"));
 
     string s;
     getline(cin, s);
 
     string result = pangrams(s);
+    cout << result;
+    // fout << result << "\n";
 
-    fout << result << "\n";
-
-    fout.close();
+    // fout.close();
 
     return 0;
 }
