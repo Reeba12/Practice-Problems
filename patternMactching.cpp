e0dbbd1cc6766f3c6b775b2a84347970c4efd757
Reeba12 <reebasiddiqui456@gmail.com>
2022-12-18 22:30:32 +0500

    feat: inverted half pyramid

--- patternMactching.cpp
+++ patternMactching.cpp
@@ -8,14 +8,14 @@ int main()
     //  ****
     // *****
     int n=5;
-    // for (int i = 1; i <=n; i++) {
-    //    for (int j = 1; j<=n; j++) {
-    //        if(j<=n-i){
-    //            cout<<" ";
-    //        }else cout<<"*";
-    //    }
-    //    cout<<endl;
-    // }
+    for (int i = 1; i <=n; i++) {
+       for (int j = 1; j<=n; j++) {
+           if(j<=n-i){
+               cout<<" ";
+           }else cout<<"*";
+       }
+       cout<<endl;
+    }
     // 1
     // 22
     // 333
@@ -27,6 +27,18 @@ int main()
        }
        cout<<endl;
     }
+    // *****
+    // ****
+    // ***
+    // **
+    // *
+    for (int i = n; i >=1; i--) {
+        for (int j = 1; j<=i; j++) {
+          cout<<"*";
+       }
+       cout<<endl;
+    }
+    
     return 0;
 }
 
