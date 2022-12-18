c14166ecb7295d580fdba1c94adab253a571866a
Reeba12 <reebasiddiqui456@gmail.com>
2022-12-19 00:11:24 +0500

    feat: butterfly pattern

--- patternMactching.cpp
+++ patternMactching.cpp
@@ -51,6 +51,61 @@ int main()
        }
        cout<<endl;
     }
+    // *      *
+    // **    **
+    // ***  ***
+    // ********
+    // ********
+    // ***  ***
+    // **    **
+    // *      *
+    // METHOD 1
+    for (int i = 1; i <=n; i++) {
+        int space=2*n;
+        for(int j=1; j<=space; j++){
+            if(j<=i || j>space-i){
+                cout<<"*";
+            }else cout<<" ";
+        }
+        cout<<endl;
+    }
+    for (int i = n; i >=1; i--) {
+        int space=2*n;
+        for(int j=1; j<=space; j++){
+            if(j<=i || j>space-i){
+                cout<<"*";
+            }else cout<<" ";
+        }
+  
+       cout<<endl;
+    }
+    // METHOD 2
+     for (int i = 1; i <=n; i++) {
+        for(int j=1; j<=i; j++){
+                cout<<"*";
+        }
+        for(int j=1; j<=2*n-2*i; j++){
+                cout<<" ";
+        }
+        for(int j=1; j<=i; j++){
+                cout<<"*";
+        }
+  
+       cout<<endl;
+    }
+     for (int i = n; i >=1; i--) {
+        for(int j=1; j<=i; j++){
+                cout<<"*";
+        }
+        for(int j=1; j<=2*n-2*i; j++){
+                cout<<" ";
+        }
+        for(int j=1; j<=i; j++){
+                cout<<"*";
+        }
+  
+       cout<<endl;
+    }
     return 0;
 }
 
